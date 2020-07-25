22c52dc18e2fb1dd1da6f044113f2616add118e1
tiammosito <tammo@Tammos-MacBook-Pro.local>
2020-07-25 14:13:24 +0200

    impletment mapFailure

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,4 +1,4 @@
-import { isNotAsked, notAsked, success, map } from '../src';
+import { isNotAsked, notAsked, success, failure, map, mapFailure } from '../src';
 
 describe('RemoteData', () => {
   it('Should be the same', () => {
@@ -9,5 +9,11 @@ describe('RemoteData', () => {
     const inc = map((data: number): number => data + 1);
 
     expect(inc(success(1))).toEqual(success(2));
+  });
+
+  it('Should map error on failure', () => {
+    const nextErr = mapFailure((error: string): string => error + "!");
+
+    expect(nextErr(failure("Something went wrong"))).toEqual(failure("Something went wrong!"));
   });
 });
